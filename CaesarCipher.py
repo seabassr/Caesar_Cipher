cf74b464605983b2c5d2e42a7842e7481cbd75ad
Sebastian Rodriguez <77996246+seabassr@users.noreply.github.com>
2022-02-24 21:03:24 -0600

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -8,72 +8,87 @@ window.title("Caesar Cipher")
 plaintext_block = tk.Frame(window)
 # Buttons for simple and complex
 encryption_block = tk.Frame(window)
-# Key dropdown, and encryption/decryption buttons
-options_block = tk.Frame(window)
 # Caesar text display
 caesartext_block = tk.Frame(window)
+# Key dropdown, and encryption/decryption buttons
+simple_options_block = tk.Frame(window)
+# Key entry, and encryption/decryption buttons
+complex_options_block = tk.Frame(window)
 
-# PLAINTEXT
+### PLAINTEXT ###
 # Plain text entry
 label_plain = tk.Label(plaintext_block, text="Plain Text:", font=14)
 label_plain.pack(side=tk.TOP)
 entry_plain = tk.Entry(plaintext_block, font=14)
 entry_plain.config(highlightbackground="grey")
-entry_plain.pack(side=tk.TOP, pady=(5, 10))
+entry_plain.pack(side=tk.TOP)
 
-# ENCRYPTION
-# Simple Caesar encryption button
-button_connect = tk.Button(encryption_block, text="Simple", font=14, command=lambda: caesar_encryption())
-button_connect.pack(side=tk.LEFT, pady=(5, 10))
-
-# Complex Caesar encryption button
-button_connect = tk.Button(encryption_block, text="Complex", font=14, command=lambda: caesar_encryption())
-button_connect.pack(side=tk.RIGHT, pady=(5, 10))
-
-# OPTIONS
-# Dropdown options
-dropbox_options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
-dropbox_menu = tk.StringVar(options_block)
-dropbox_menu.set(dropbox_options[0])
-key = dropbox_options[0]
-
-# Update key, when dropdown is used
-def update_key(selection):
-    global key
-    key = selection
-
-# Key dropdown
-label_key = tk.Label(options_block, text="Key:", font=14)
-label_key.pack(side=tk.TOP)
-entry_key = tk.OptionMenu(options_block, dropbox_menu, *dropbox_options, command=update_key)
-entry_key.pack(side=tk.TOP, pady=(5, 10))
-
-
-# Caesar encryption button
-button_connect = tk.Button(options_block, text="Encrypt!", font=14, command=lambda: caesar_encryption())
-button_connect.pack(side=tk.LEFT, pady=(5, 10))
-
-# Caesar decryption button
-button_connect = tk.Button(options_block, text="Decrypt!", font=14, command=lambda: caesar_decryption())
-button_connect.pack(side=tk.RIGHT, pady=(5, 10))
-
-# CAESARTEXT
+### CAESARTEXT ###
 # Caesar text display
 label_caesar = tk.Label(caesartext_block, text="Caesar Text:", font=14)
 label_caesar.pack(side=tk.TOP)
 entry_caesar = tk.Entry(caesartext_block, font=14)
 entry_caesar.config(highlightbackground="grey", state="disabled")
-entry_caesar.pack(side=tk.TOP, pady=(5, 10))
+entry_caesar.pack(side=tk.TOP)
 
-# Display blocks
+### ENCRYPTION ###
+# Simple Caesar encryption button
+button_connect = tk.Button(encryption_block, text="Simple", font=14, command=lambda: show_simple_options())
+button_connect.pack(side=tk.LEFT)
+
+# Complex Caesar encryption button
+button_connect = tk.Button(encryption_block, text="Complex", font=14, command=lambda: show_complex_options())
+button_connect.pack(side=tk.RIGHT)
+
+### SIMPLE ENCRYPTION OPTIONS ###
+# Dropdown options
+dropbox_options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+dropbox_menu = tk.StringVar(simple_options_block)
+dropbox_menu.set(dropbox_options[0])
+simple_key = dropbox_options[0]
+
+# Update key, when dropdown is used
+def update_key(selection):
+    global simple_key
+    simple_key = selection
+
+# Key dropdown
+label_drop_key = tk.Label(simple_options_block, text="Key:", font=14)
+label_drop_key.pack(side=tk.TOP)
+drop_key = tk.OptionMenu(simple_options_block, dropbox_menu, *dropbox_options, command=update_key)
+drop_key.pack(side=tk.TOP)
+
+# Caesar encryption button
+button_connect = tk.Button(simple_options_block, text="Encrypt!", font=14, command=lambda: simple_caesar_encryption())
+button_connect.pack(side=tk.LEFT)
+
+# Caesar decryption button
+button_connect = tk.Button(simple_options_block, text="Decrypt!", font=14, command=lambda: simple_caesar_decryption())
+button_connect.pack(side=tk.RIGHT)
+
+### COMPLEX ENCRYPTION OPTIONS ###
+# Key dropdown
+label_entry_key = tk.Label(complex_options_block, text="Key:", font=14)
+label_entry_key.pack(side=tk.TOP)
+entry_key = tk.Entry(complex_options_block, font=14)
+entry_key.pack(side=tk.TOP)
+
+# Caesar encryption button
+button_connect = tk.Button(complex_options_block, text="Encrypt!", font=14)
+button_connect.pack(side=tk.LEFT)
+
+# Caesar decryption button
+button_connect = tk.Button(complex_options_block, text="Decrypt!", font=14)
+button_connect.pack(side=tk.RIGHT)
+
+### DISPLAYING BLOCKS ###
 plaintext_block.pack(side=tk.TOP, padx=(15, 15), pady=(10, 10))
-encryption_block.pack(side=tk.TOP, padx=(15, 15), pady=(10, 10))
-options_block.pack(side=tk.TOP, padx=(15, 15), pady=(10, 10))
 caesartext_block.pack(side=tk.TOP, padx=(15, 15), pady=(10, 10))
+encryption_block.pack(side=tk.TOP, padx=(15, 15), pady=(10, 10))
 
 # Simple caesar encryption
-def caesar_encryption():
-    global key
+def simple_caesar_encryption():
+    global simple_key
     plaintext = entry_plain.get()
     caesartext = ""
 
@@ -83,7 +98,7 @@ def caesar_encryption():
         if new_special == " ":
             caesartext += ' '
         elif special.isalpha():
-            caesartext += chr((ord(new_special) + key - 97) % 26 + 97)
+            caesartext += chr((ord(new_special) + simple_key - 97) % 26 + 97)
 
     entry_caesar.config(state=tk.NORMAL)
     entry_caesar.delete(0, tk.END)
@@ -91,8 +106,8 @@ def caesar_encryption():
     entry_caesar.config(state=tk.DISABLED)
 
 # Simple caesar decryption
-def caesar_decryption():
-    global key
+def simple_caesar_decryption():
+    global simple_key
     plaintext = entry_plain.get()
     caesartext = ""
 
@@ -102,12 +117,20 @@ def caesar_decryption():
         if new_special == " ":
             caesartext += ' '
         elif special.isalpha():
-            caesartext += chr((ord(new_special) - key - 97) % 26 + 97)
+            caesartext += chr((ord(new_special) - simple_key - 97) % 26 + 97)
 
     entry_caesar.config(state=tk.NORMAL)
     entry_caesar.delete(0, tk.END)
     entry_caesar.insert(tk.END, caesartext)
     entry_caesar.config(state=tk.DISABLED)
+
+def show_simple_options():
+    complex_options_block.forget()
+    simple_options_block.pack(side=tk.BOTTOM, padx=(15, 15), pady=(10, 15))
+
+def show_complex_options():
+    simple_options_block.forget()
+    complex_options_block.pack(side=tk.BOTTOM, padx=(15, 15), pady=(10, 15))
 
 # Loop GUI window
 window.mainloop()
